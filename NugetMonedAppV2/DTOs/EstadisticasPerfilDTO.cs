2e479107a19980fe91a4a81b677c95d87533da32
amandacrespo <mancreslu@hotmail.com>
2025-04-06 14:03:47 +0200

    Cambio model a dto para su uso correcto (estadisticas perfil)

--- NugetMonedAppV2/DTOs/EstadisticasPerfilDTO.cs
+++ NugetMonedAppV2/DTOs/EstadisticasPerfilDTO.cs
@@ -1,15 +1,19 @@
-﻿namespace NugetMonedAppV2.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetMonedAppV2.DTOs
 {
-    public class EstadisticasPerfil
+    public class EstadisticasPerfilDTO
     {
         public int TotalCuentas { get; set; }
         public int CuentasActivas { get; set; }
         public int CuentasInactivas { get; set; }
-
         public int TotalFondos { get; set; }
         public decimal TotalMontoReservado { get; set; }
         public decimal TotalMontoGastado { get; set; }
-
         public int TotalTransacciones { get; set; }
         public decimal TotalIngresos { get; set; }
         public decimal TotalGastos { get; set; }
