0efb27d738ab7971409283a3b266e41e4ec238ba
amandacrespo <mancreslu@hotmail.com>
2025-04-06 13:28:48 +0200

    Subida version 4.0.0 con dtos del perfil de usuario

---
+++ NugetMonedAppV2/DTOs/ActualizarPerfilDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace NugetMonedAppV2.DTOs
+{
+    public class ActualizarPerfilDTO
+    {
+        public string Nombre { get; set; }
+        public string CorreoElectronico { get; set; }
+    }
+}
---
+++ NugetMonedAppV2/DTOs/UsuarioDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace NugetMonedAppV2.DTOs
+{
+    public class UsuarioDTO
+    {
+        public string Nombre { get; set; }
+        public string Dni { get; set; }
+        public string CorreoElectronico { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
